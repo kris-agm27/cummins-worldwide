cd17c67ad93fc8c147b75e9f2934f55ae9e634dd
kris-agm27 <97750079+kris-agm27@users.noreply.github.com>
2026-07-27 18:54:08 +0800

    Update site-chrome.tsx

--- app/site-chrome.tsx
+++ app/site-chrome.tsx
@@ -71,7 +71,7 @@ export function PageHero({
             sizes="(max-width: 900px) 100vw, 48vw"
             style={{
               objectFit: "cover",
-              objectPosition: " 50% center",
+              objectPosition: " 40% center",
             }}
           />
         </div>
